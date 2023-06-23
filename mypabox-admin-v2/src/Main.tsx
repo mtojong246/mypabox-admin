1dd2471cc08db087ea4b17f4facfcaa388b72311
jacknimmble6 <79076036+jacknimmble6@users.noreply.github.com>
2023-06-23 00:49:16 -0400

    Change Main.jsx to main.tsx

--- mypabox-admin-v2/src/Main.tsx
+++ mypabox-admin-v2/src/Main.tsx
@@ -5,12 +5,10 @@ import { Link, useNavigate } from 'react-router-dom'
 const Main = () => {
   const navigate = useNavigate()
 
-  //Clears the username in local storage
-  //Clears the password in local storage
-  //Navigates user to login page
+  //Clears local storage
+  //Navigates user from main page to login page
   const signOut = () => {
-    localStorage.clear('username')
-    localStorage.clear('password')
+    localStorage.clear()
     navigate('/')
   }
 
@@ -18,16 +16,16 @@ const Main = () => {
     <div>
       <div className='bg-blue-500 h-16 w-full'>
         <img src={logo} alt='myPAbox' className='h-16'/>
-        <Link href='/schools' className='absolute text-white text-xl top-4 left-[400px]'>
+        <Link to='/schools' className='absolute text-white text-xl top-4 left-[400px]'>
           Schools
         </Link>
-        <Link href='/courses' className='absolute text-white text-xl top-4 left-[550px]'>
+        <Link to='/courses' className='absolute text-white text-xl top-4 left-[550px]'>
           Courses
         </Link>
-        <Link href='/categories' className='absolute text-white text-xl top-4 left-[700px]'>
+        <Link to='/categories' className='absolute text-white text-xl top-4 left-[700px]'>
           Course Categories
         </Link>
-        <Link href='/users' className='absolute text-white text-xl top-4 left-[950px]'>
+        <Link to='/users' className='absolute text-white text-xl top-4 left-[950px]'>
           Users
         </Link>
         <button onClick={signOut} className='absolute top-4 left-[1300px] text-xl 
