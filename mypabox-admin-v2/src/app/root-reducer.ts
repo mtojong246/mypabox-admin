20ee478aebe5e0fc149aa2320937784c87b6980f
jacknimmble6 <79076036+jacknimmble6@users.noreply.github.com>
2023-07-18 12:31:20 -0400

    Update root-reducer.ts

--- mypabox-admin-v2/src/app/root-reducer.ts
+++ mypabox-admin-v2/src/app/root-reducer.ts
@@ -2,6 +2,15 @@ import { combineReducers } from "redux";
 import { addSchoolReducer } from "./slices/addSchool";
 import { loginReducer } from "./slices/login";
 import { schoolReducer } from "./slices/schools";
+import { loginState } from "../types/login.types";
+import { SchoolState } from "../types/schools.types";
+import { addSchoolState } from "../types/addSchool.types";
+
+export interface AppState {
+  login: loginState,
+  schools: SchoolState,
+  addSchool: addSchoolState
+}
 
 // combines all reducers into one root reducer
 export const rootReducer = combineReducers({
