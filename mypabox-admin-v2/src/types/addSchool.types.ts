9b5f9efe122c3500d65a02b8db62c68f2316c6e5
jacknimmble6 <79076036+jacknimmble6@users.noreply.github.com>
2023-07-18 12:32:10 -0400

    Update addSchool.types.ts

--- mypabox-admin-v2/src/types/addSchool.types.ts
+++ mypabox-admin-v2/src/types/addSchool.types.ts
@@ -4,118 +4,39 @@ export type Note = {
 }
 
 export type addSchoolState = {
-  schoolName: { 
-    schoolName: string,
-    notes: Note[]
-  },
-  schoolLogo: { 
-    schoolLogo: string,
-    notes: Note[] 
-  },
-  streetAddress: { 
-    streetAdress: string,
-    notes: Note[]
-  },
-  city: { 
-    city: string,
-    notes: Note[]
-  },
-  state: { 
-    state: string,
-    notes: Note[]
-  },
-  zip: { 
-    zip: string,
-    notes: Note[]
-  },
-  country: { 
-    country: string,
-    notes: Note[]
-  },
-  website: { 
-    website: string,
-    notes: Note[] 
-  },
-  schoolEmail: { 
-    schoolEmail: string,
-    notes: Note[]
-  },
-  schoolPhoneNumber: { 
-    schoolPhoneNumber: string,
-    notes: Note[] 
-  },
-  campusLocation: { 
-    campusLocation: string,
-    notes: Note[]
-  },
-  startMonth: { 
-    startMonth: string,
-    notes: Note[]
-  },
-  classCapacity: {
-    classCapacity: number,
-    notes: Note[]
-  }
-  fullTimeDuration: { 
-    fullTimeDuration: string,
-    notes: Note[]
-  },
-  partTimeDuration: { 
-    partTimeDuration: string,
-    notes: Note[]
-  },
-  inStateDeposit: {
-    inStateDeposit: number,
-    notes: Note[]
-  }
-  outOfStateDeposit: {
-    outOfStateDeposit: number,
-    notes: Note[]
-  }
-  rollingAdmissions: {
-    rollingAdmissions: boolean,
-    notes: Note[]
-  }
-  nonRollingAdmissions: {
-    nonRollingAdmissions: boolean,
-    notes: Note[]
-  } 
-  prePACurriculum: {
-    prePACurriculum: boolean,
-    notes: Note[]
-  }
-  directHighSchoolEntry: {
-    directHighSchoolEntry: boolean,
-    notes: Note[]
-  }
-  partTimeOption: {
-    partTimeOption: boolean,
-    notes: Note[]
-  }
-  onlineLearning: {
-    onlineLearning: boolean,
-    notes: Note[]
-  }
-  onCampusHousing: { 
-    onCampusHousing: boolean,
-    notes: Note[]
-  }
-  cadaverLab: {
-    cadaverLab: boolean,
-    notes: Note[]
-  }
-  faithBasedLearning: {
-    faithBasedLearning: boolean,
-    notes: Note[]
-  }
-  militaryPersonnelPreference: {
-    militaryPersonnelPreference: boolean,
-    notes: Note[]
-  }
-  generalInformationnotess: { 
-    generalInformationnotess: string,
-    notes: Note[]
-  }
+  id: number,
+  school_name: string;
+    school_logo: string;
+    school_street: string;
+    school_city: string;
+    school_state: string;
+    school_zip_code: string;
+    school_country: string;
+    school_website: string;
+    school_email: string;
+    school_phone_number: string;
+    school_campus_location: string;
+    school_start_month: string;
+    school_class_capacity: number;
+    school_duration_full_time: string;
+    school_duration_part_time: string;
+    school_seat_deposit_in_state: number;
+    school_seat_deposit_out_of_state: number;
+    school_rolling_admissions: boolean;
+    school_nonrolling_admissions: boolean;
+    school_pre_pa_curriculum: boolean;
+    school_direct_high_school_entry: boolean;
+    school_part_time_option: boolean;
+    school_online_learning: boolean;
+    school_on_campus_housing: boolean;
+    school_cadaver_lab: boolean;
+    school_faith_based_learning: boolean;
+    school_military_personnel_preference: boolean;
+    school_general_information: string;
+    school_dual_degree_program: boolean;
+    school_type_of_degree_offered: string;
+    school_bachelors_degree_required: boolean;
+
 }
 
 export type addSchoolNameAction = {
