21c75bec304a76177011a7887b057d72daf5f913
jacknimmble6 <79076036+jacknimmble6@users.noreply.github.com>
2023-07-18 12:31:55 -0400

    Update addSchool.ts

--- mypabox-admin-v2/src/app/slices/addSchool.ts
+++ mypabox-admin-v2/src/app/slices/addSchool.ts
@@ -1,135 +1,152 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { Note, addSchoolNameAction, addSchoolState } from "../../types/addSchool.types";
+import { School } from "../../types/schools.types";
 
 // The initial state for the login slice
-const initialState: addSchoolState = {
-    schoolName: {
-        schoolName: "",
-        notes: []
+const initialState: School= {
+    id: 0,
+    school_name: {
+        input: "",
+        notes: [],
     },
-    schoolLogo: {
-        schoolLogo: "",
-        notes: []
+    school_logo: {
+        input: "",
+        notes: [],
     },
-    streetAddress: {
-        streetAdress: "",
-        notes: []
+    school_street: {
+        input: "",
+        notes: [],
     },
-    city: {
-        city: "",
-        notes: []
+    school_city: {
+        input: "",
+        notes: [],
     },
-    state: {
-        state: "",
-        notes: []
+    school_state: {
+        input: "",
+        notes: [],
     },
-    zip: {
-        zip: "",
-        notes: []
+    school_zip_code: {
+        input: "",
+        notes: [],
     },
-    country: {
-        country: "",
-        notes: []
+    school_country: {
+        input: "",
+        notes: [],
     },
-    website: {
-        website: "",
-        notes: []
+    school_website: {
+        input: "",
+        notes: [],
     },
-    schoolEmail: {
-        schoolEmail: "",
-        notes: []
+    school_email: {
+        input: "",
+        notes: [],
     },
-    schoolPhoneNumber: {
-        schoolPhoneNumber: "",
-        notes: []
+    school_phone_number: {
+        input: "",
+        notes: [],
     },
-    campusLocation: {
-        campusLocation: "",
-        notes: []
+
+    school_campus_location: {
+        input: "",
+        notes: [],
     },
-    startMonth: {
-        startMonth: "",
-        notes: []
+    school_start_month: {
+        input: "",
+        notes: [],
     },
-    classCapacity: {
-        classCapacity: 0,
-        notes: []
+    school_class_capacity: {
+        input: 0,
+        notes: [],
     },
-    fullTimeDuration: {
-        fullTimeDuration: "",
-        notes: []
+    school_duration_full_time: {
+        input: "",
+        notes: [],
     },
-    partTimeDuration: {
-        partTimeDuration: "",
-        notes: []
+    school_duration_part_time: {
+        input: "",
+        notes: [],
     },
-    inStateDeposit: {
-        inStateDeposit: 0,
-        notes: []
+
+    school_seat_deposit_in_state: {
+        input: 0,
+        notes: [],
     },
-    outOfStateDeposit: {
-        outOfStateDeposit: 0,
-        notes: []
+    school_seat_deposit_out_of_state: {
+        input: 0,
+        notes: [],
     },
-    rollingAdmissions: {
-        rollingAdmissions: false,
-        notes: []
+
+    school_rolling_admissions: {
+        input: false,
+        notes: [],
     },
-    nonRollingAdmissions: {
-        nonRollingAdmissions: false,
-        notes: []
+    school_nonrolling_admissions: {
+        input: false,
+        notes: [],
     },
-    prePACurriculum: {
-        prePACurriculum: false,
-        notes: []
+    school_pre_pa_curriculum: {
+        input: false,
+        notes: [],
     },
-    directHighSchoolEntry: {
-        directHighSchoolEntry: false,
-        notes: []
+    school_direct_high_school_entry: {
+        input: false,
+        notes: [],
     },
-    partTimeOption: {
-        partTimeOption: false,
-        notes: []
+    school_part_time_option: {
+        input: false,
+        notes: [],
     },
-    onlineLearning: {
-        onlineLearning: false,
-        notes: []
+    school_online_learning: {
+        input: false,
+        notes: [],
     },
-    onCampusHousing: {
-        onCampusHousing: false,
-        notes: []
+    school_on_campus_housing: {
+        input: false,
+        notes: [],
     },
-    cadaverLab: {
-        cadaverLab: false,
-        notes: []
+    school_cadaver_lab: {
+        input: false,
+        notes: [],
     },
-    faithBasedLearning: {
-        faithBasedLearning: false,
-        notes: []
+    school_faith_based_learning: {
+        input: false,
+        notes: [],
     },
-    militaryPersonnelPreference: {
-        militaryPersonnelPreference: false,
-        notes: []
+    school_military_personnel_preference: {
+        input: false,
+        notes: [],
     },
-    generalInformationnotess: {
-        generalInformationnotess: "",
-        notes: []
-    }
+
+    school_general_information: "",
+
+    school_dual_degree_program: {
+        input: false,
+        notes: [],
+    },
+    school_type_of_degree_offered: {
+        fields: [{
+            input: "",
+        }],
+        notes: [],
+    },
+    school_bachelors_degree_required: {
+        input: false,
+        notes: [],
+    },
 }
 
 const addSchoolSlice = createSlice({
   name: 'loginSlice',
   initialState,
   reducers: {
-    addSchoolName: (state, action) => {
-      state.schoolName.schoolName = action.payload?.schoolName
-      state.schoolName.notes?.push(action.payload.notes)
+    addSchool: (state, action) => {
+      state = action.payload
     },
     clear: state => {
     }
   }
 })
 
-export const { addSchoolName } = addSchoolSlice.actions
+export const { addSchool } = addSchoolSlice.actions
 
 export const addSchoolReducer = addSchoolSlice.reducer
