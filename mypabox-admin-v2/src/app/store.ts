99d2b4795441741ef24a0736cbc106537b885994
jacknimmble6 <79076036+jacknimmble6@users.noreply.github.com>
2023-07-18 12:32:01 -0400

    Update store.ts

--- mypabox-admin-v2/src/app/store.ts
+++ mypabox-admin-v2/src/app/store.ts
@@ -4,7 +4,6 @@ import storage from 'redux-persist/lib/storage';
 import { persistReducer, persistStore } from 'redux-persist';
 import thunk from "redux-thunk";
 
-
 export type RootState = ReturnType<typeof rootReducer>
 
 const persistConfig = {
