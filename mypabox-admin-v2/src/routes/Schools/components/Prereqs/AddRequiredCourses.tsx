ba7726f382a718dcb08333aaf340a83e83e61775
Melanie Tojong <mtojong@Melanies-MacBook-Pro.local>
2023-09-09 13:25:36 -0400

    adding ui for required course

--- mypabox-admin-v2/src/routes/Schools/components/Prereqs/AddRequiredCourses.tsx
+++ mypabox-admin-v2/src/routes/Schools/components/Prereqs/AddRequiredCourses.tsx
@@ -1,7 +1,65 @@
 import Select from 'react-select';
 import ReactQuill from "react-quill";
+import { Dispatch, SetStateAction, useState, useEffect, ChangeEvent } from 'react';
+import { School } from '../../../../types/schools.types';
+import { useSelector } from 'react-redux';
+import { selectCourses } from '../../../../app/selectors/courses.selectors';
+import { SchoolPrereqRequiredCourse } from '../../../../types/schools.types';
 
-export default function AddRequiredCourses({ toggleRequiredCourses }: { toggleRequiredCourses: () => void }) {
+const defaultCourse = {
+    school_required_course_id: '',
+    school_required_course_lab: false,
+    school_required_course_lab_preferred: false,
+    school_required_course_credit_hours: 0,
+    school_required_course_quarter_hours: 0,
+    school_required_course_note_section: '',
+}
+
+export default function AddRequiredCourses({ toggleRequiredCourses, addRequiredCourse }: { toggleRequiredCourses: () => void, addRequiredCourse: (course: SchoolPrereqRequiredCourse) => void }) {
+    const courses = useSelector(selectCourses)
+    const [ courseOptions, setCourseOptions ] = useState<{ value: string, label: string }[]>([]);
+    const [ requiredCourse, setRequiredCourse ] = useState<SchoolPrereqRequiredCourse>(defaultCourse);
+    const [ selection, setSelection ] = useState<string | undefined>('');
+
+    useEffect(() => {
+        setCourseOptions(courses.map(course => (
+            { value: course.course_name, label: course.course_name }
+        )))
+    }, [courses])
+
+    useEffect(() => {
+        if (selection) {
+            const selectedCourse = courses.find(course => course.course_name === selection)
+            if (selectedCourse) {
+                setRequiredCourse({
+                    ...requiredCourse,
+                    school_required_course_id: selectedCourse.unique_id,
+                })
+            }
+        }
+    }, [selection, courses])
+
+    const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+        setRequiredCourse({
+            ...requiredCourse,
+            [e.target.name]: e.target.value, 
+        })
+    }
+
+    const handleCheck = (e: ChangeEvent<HTMLInputElement>) => {
+        setRequiredCourse({
+            ...requiredCourse, 
+            [e.target.name]: e.target.checked
+        })
+    }
+
+    const handleNote = (e: any) => {
+        setRequiredCourse({
+            ...requiredCourse,
+            school_required_course_note_section: e,
+        })
+    }
+
     return (
         <div className='fixed top-0 left-0 right-0 bottom-0 z-10'>
             <div className='fixed bg-[rgba(0,0,0,0.2)] top-0 left-0 right-0 bottom-0 flex justify-center items-center p-10'>
@@ -9,33 +67,33 @@ export default function AddRequiredCourses({ toggleRequiredCourses }: { toggleRe
                     <p className='text-xl font-semibold mb-8'>Add Required Course</p>
                     <div className='w-full mb-8'>
                         <label className='font-medium'>Course name:</label>
-                        <Select className='w-full focus:outline-none rounded mt-2'/>
+                        <Select onChange={(e) => setSelection(e?.value)} value={selection ? { value: selection, label: selection } : null} className='w-full focus:outline-none rounded mt-2' options={courseOptions}/>
                     </div>
                     <div className='w-full mb-8 flex justify-start items-center gap-10'>
                         <div>
-                            <input type='checkbox' className='mr-2' />
+                            <input type='checkbox' name='school_required_course_lab' className='mr-2' onChange={handleCheck}/>
                             <label className='font-medium'>With Lab</label>
                         </div>
                         <div>
-                            <input type='checkbox' className='mr-2' />
+                            <input type='checkbox' name='school_required_course_lab_preferred' className='mr-2' onChange={handleCheck}/>
                             <label className='font-medium'>Lab Preferred</label>
                         </div>
                     </div>
                     <div className='w-full mb-8'>
                         <label className='font-medium'>Credits:</label>
-                        <input className='w-32 focus:outline-none border border-[#B4B4B4] py-2 px-3 rounded mt-2 block' />
+                        <input onChange={handleInput} value={requiredCourse.school_required_course_credit_hours} name='school_required_course_credit_hours' className='w-32 focus:outline-none border border-[#B4B4B4] py-2 px-3 rounded mt-2 block' />
                     </div>
                     <div className='w-full mb-8'>
                         <label className='font-medium'>Quarter hours:</label>
-                        <input className='w-32 focus:outline-none border border-[#B4B4B4] py-2 px-3 rounded mt-2 block' />
+                        <input onChange={handleInput} value={requiredCourse.school_required_course_quarter_hours} name='school_required_course_quarter_hours' className='w-32 focus:outline-none border border-[#B4B4B4] py-2 px-3 rounded mt-2 block' />
                     </div>
                     <div className='w-full mb-14'>
                         <label className='font-medium'>Note:</label>
-                        <ReactQuill className='mt-2 h-[200px] rounded w-full' theme="snow"/>
+                        <ReactQuill className='mt-2 h-[200px] rounded w-full' theme="snow" onChange={handleNote}/>
                     </div>
                     <div className='w-full flex justify-end items-center gap-3'>
                         <button onClick={toggleRequiredCourses} className='border-2 border-[#B4B4B4] bg-none text-[#B4B4B4] font-medium px-3 py-2 rounded-md'>Cancel</button>
-                        <button className='border-2 border-[#4573D2] bg-[#4573D2] text-white font-medium px-3 py-2 rounded-md'>Add course</button>
+                        <button onClick={() => { toggleRequiredCourses(); addRequiredCourse(requiredCourse) }} className='border-2 border-[#4573D2] bg-[#4573D2] text-white font-medium px-3 py-2 rounded-md'>Add course</button>
                     </div>
                 </div>
             </div>
--- mypabox-admin-v2/src/routes/Schools/components/Prereqs/Prereqs.tsx
+++ mypabox-admin-v2/src/routes/Schools/components/Prereqs/Prereqs.tsx
@@ -1,11 +1,17 @@
 import { Dispatch, SetStateAction, useState } from "react";
-import { School } from "../../../../types/schools.types";
+import { School, SchoolPrereqRecommendedCourse, SchoolPrereqRequiredCourse } from "../../../../types/schools.types";
 import AddRequiredCourses from "./AddRequiredCourses";
 import AddRequiredCourseCategories from "./AddRequiredCourseCategories";
 import AddRequiredOptionalCourses from "./AddRequiredOptionalCourses";
 import AddRecommendedCourses from "./AddRecommendedCourses";
+import { FiEdit3 } from 'react-icons/fi'
+import { AiOutlineClose } from 'react-icons/ai'
+import ReactQuill from "react-quill";
+import { useSelector } from "react-redux";
+import { selectCourses } from "../../../../app/selectors/courses.selectors";
 
 export default function Prereqs({ newSchool, setNewSchool }: { newSchool: School, setNewSchool: Dispatch<SetStateAction<School>> }) {
+    const courses = useSelector(selectCourses)
     const [ openRequiredCourses, setOpenRequiredCourses ] = useState(false);
     const [ openRequiredCourseCategories, setOpenRequiredCourseCategories ] = useState(false);
     const [ openRequiredOptionalCourses, setOpenRequiredOptionalCourses ] = useState(false);
@@ -16,15 +22,57 @@ export default function Prereqs({ newSchool, setNewSchool }: { newSchool: School
     const toggleRequiredOptionalCourses = () => setOpenRequiredOptionalCourses(!openRequiredOptionalCourses);
     const toggleRecommendedCourses = () => setOpenRecommendedCourses(!openRecommendedCourses);
 
+    const addRequiredCourse = (course: SchoolPrereqRequiredCourse) => {
+        setNewSchool({
+            ...newSchool,
+            school_prereq_required_courses: newSchool.school_prereq_required_courses.concat(course)
+        })
+    }
+
+    console.log(newSchool.school_prereq_required_courses)
+
     return (
         <>
         {newSchool && (
             <>
-                <div className={`mt-10 relative max-w-[900px] border p-5 block rounded-lg border-[#B4B4B4]`}>
+                <div className={`mt-10 relative max-w-[900px] border p-5 block rounded border-[#B4B4B4]`}>
                     <label className="absolute top-[-16px] text-xl bg-white">Required Courses</label>   
                     <button onClick={toggleRequiredCourses} className="border text-[#F06A6A] border-[#F06A6A] rounded-md h-14 px-5 text-xl hover:text-white hover:bg-[#F06A6A]">
                         Add Course
                     </button>
+                    <div className={`flex flex-col justify-center items-center gap-4 ${newSchool.school_prereq_required_courses.length ? 'mt-5' : 'mt-0'}`}>
+                    {courses && newSchool.school_prereq_required_courses.map(course => {
+                        const selectedCourse = courses.find(c => c.unique_id === course.school_required_course_id)
+                        if (selectedCourse) {
+                            return (
+                                <div className='py-2 pr-2 pl-5 border border-[#B4B4B4] rounded w-full'>
+                                    <div className='flex justify-between items-center w-full'>
+                                        <p className='font-bold'>{selectedCourse?.course_name}
+                                            <span className='font-semibold text-sm inline-block pl-3 text-[#6A6A6A]'>
+                                                {`(${course.school_required_course_lab ? 'with lab' : 'without lab'}
+                                                 ${!course.school_required_course_lab && course.school_required_course_lab_preferred ? ' / lab preferred' : ''}  
+                                                / ${course.school_required_course_credit_hours} credit hours 
+                                                / ${course.school_required_course_quarter_hours} quarter hours)`}                                   
+                                            </span>
+                                        </p>
+                                        <div className='flex gap-2'>
+                                            <button><FiEdit3 className='h-7 w-7 border-2 rounded-md border-[#4573D2] bg-none text-[#4573D2]'/></button>
+                                            <button><AiOutlineClose className='h-7 w-7 border-2 rounded-md border-[#F06A6A] bg-none text-[#F06A6A]'/></button>
+                                        </div>
+                                    </div>
+                                    {course.school_required_course_note_section ? (
+                                        <>
+                                            <p className='font-semibold underline underline-offset-2 mt-3 mb-1'>Note:</p>
+                                            <ReactQuill theme='bubble' value={course.school_required_course_note_section} readOnly={true} className='edited-quill'/>
+                                        </>
+                                    ) : null}
+                                </div>
+                            )
+                        } else {
+                            return null
+                        }
+                    })}
+                    </div>
                 </div>
                 <div className={`mt-28 relative max-w-[900px] border p-5 block rounded-lg border-[#B4B4B4]`}>
                     <label className="absolute top-[-16px] text-xl bg-white">Required Optional Courses</label>   
@@ -46,7 +94,7 @@ export default function Prereqs({ newSchool, setNewSchool }: { newSchool: School
                 </div>
             </>
         )}
-        {openRequiredCourses && <AddRequiredCourses toggleRequiredCourses={toggleRequiredCourses}/>}
+        {openRequiredCourses && <AddRequiredCourses toggleRequiredCourses={toggleRequiredCourses} addRequiredCourse={addRequiredCourse}/>}
         {openRequiredCourseCategories && <AddRequiredCourseCategories toggleRequiredCourseCategories={toggleRequiredCourseCategories}/>}
         {openRequiredOptionalCourses && <AddRequiredOptionalCourses toggleRequiredOptionalCourses={toggleRequiredOptionalCourses}/>}
         {openRecommendedCourses && <AddRecommendedCourses toggleRecommendedCourses={toggleRecommendedCourses}/>}
