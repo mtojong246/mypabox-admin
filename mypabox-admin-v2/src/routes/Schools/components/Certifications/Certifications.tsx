c549157ff5f25e0681132a4f21c1797cb118c1fc
Melanie Tojong <mtojong@Melanies-MBP.lan>
2023-11-17 10:28:39 -0500

    changing ui for certifications section

--- mypabox-admin-v2/src/routes/Schools/components/Certifications/Certifications.tsx
+++ mypabox-admin-v2/src/routes/Schools/components/Certifications/Certifications.tsx
@@ -119,12 +119,17 @@ export default function Certifications({ newSchool, setNewSchool }: { newSchool:
         <>
         <div className={`mt-10 relative max-w-[900px] border-2 p-4 block rounded border-[#B4B4B4]`}>
             <label className="absolute top-[-16px] text-xl bg-white">Certifications Required</label>  
-            <div className='w-full mt-2'>
+            <div className={`w-full ${!newSchool.school_certifications_required.input ? 'flex justify-between items-center mt-0' : 'block mt-2'}`}>
                 <label className="relative inline-flex items-center cursor-pointer">
                     <input onChange={handleCheck} checked={newSchool.school_certifications_required.input ? true : false} type="checkbox" className="sr-only peer"/>
                     <div className="w-12 h-8 bg-gray-200 peer-focus:outline-none rounded-full shadow-inner peer dark:bg-gray-200 peer-checked:after:translate-x-[16px] after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:rounded-full after:h-7 after:w-7 after:transition-all peer-checked:bg-orange-600"></div>
                     <span className="ml-3 text-xl text-black">{newSchool.school_certifications_required.input ? 'True' : 'False'}</span>
                 </label>
+                {!newSchool.school_certifications_required.input && (
+                    <button onClick={toggleNotePopup} className="block border text-[#F06A6A] border-[#F06A6A] rounded h-[50px] px-5 text-xl hover:text-white hover:bg-[#F06A6A]">
+                        Add Note
+                    </button>
+                )}
             </div>
             {newSchool.school_certifications_required.input && (
                 <div className={`mt-8 mx-4 relative max-w-[900px] p-4 block rounded border-[#545454] border-2`}>
@@ -174,6 +179,22 @@ export default function Certifications({ newSchool, setNewSchool }: { newSchool:
                 </div>
             )}
             </div>
+            )}
+            {newSchool.school_certifications_required.school_certification_notes && !newSchool.school_certifications_required.input && (
+                <div className={`flex flex-col justify-center items-center gap-3 ${newSchool.school_certifications_required.school_certification_notes.length ? 'mt-3' : 'mt-0'}`}>
+                    {newSchool.school_certifications_required.school_certification_notes.map((note, i) => (
+                        <div className='py-2 pr-2 pl-3 border border-[#B4B4B4] rounded w-full'>
+                            <div className='flex justify-between items-center w-full mb-1'>
+                                <p className={`font-semibold ${note.type === 'information' ? 'text-[#4573D2]' : 'text-[#F06A6A]'}`}>{note.type}:</p>
+                                <div className='flex gap-2'>
+                                    <button onClick={(e) => {toggleNotePopup(e); setEditedNote(note); setIndex(i);}}><FiEdit3 className='h-7 w-7 border-2 rounded-md border-[#4573D2] bg-none text-[#4573D2] hover:text-white hover:bg-[#4573D2]'/></button>
+                                    <button onClick={(e) => deleteNote(e, i)}><AiOutlineClose className='h-7 w-7 border-2 rounded-md border-[#F06A6A] bg-none text-[#F06A6A] hover:text-white hover:bg-[#F06A6A]'/></button>
+                                </div>
+                            </div>
+                            <ReactQuill theme='bubble' value={note.note} readOnly={true} className='edited-quill'/>
+                        </div>
+                    ))}
+                </div>
             )}
         </div>
         {notePopup && <AddNote toggleNotePopup={toggleNotePopup} addNote={addNote} editedNote={editedNote} setEditedNote={setEditedNote} updateNote={updateNote}/>}
