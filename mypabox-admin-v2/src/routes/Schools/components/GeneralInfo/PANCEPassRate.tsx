1adada3866ff7d7ba830e033b43348faa3dc10fb
Melanie Tojong <mtojong@Melanies-MBP.lan>
2023-11-23 10:40:43 -0500

    adding abiliyt to delete all text in input

--- mypabox-admin-v2/src/routes/Schools/components/GeneralInfo/PANCEPassRate.tsx
+++ mypabox-admin-v2/src/routes/Schools/components/GeneralInfo/PANCEPassRate.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, Dispatch, SetStateAction, useState, KeyboardEvent } from "react";
+import { ChangeEvent, Dispatch, SetStateAction, useState, KeyboardEvent, SyntheticEvent } from "react";
 import { School, Note, StringInput, NumberInput, BooleanInput } from "../../../../types/schools.types";
 import ReactQuill from "react-quill";
 import { AiOutlineClose } from "react-icons/ai";
@@ -10,6 +10,9 @@ export default function PANCEPassRate({newSchool, setNewSchool}: { newSchool: Sc
     const [editedNote, setEditedNote] = useState<Note | null>(null);
     const [notePopup, setNotePopup] = useState(false);
     const [name, setName] = useState('');
+
+    const [firstSelected, setFirstSelected] = useState('');
+    const [fiveSelected, setFiveSelected] = useState('');
 
     const toggleNotePopup = (e: any) => {
         e.preventDefault();
@@ -59,27 +62,67 @@ export default function PANCEPassRate({newSchool, setNewSchool}: { newSchool: Sc
 
     const keyDownFirst = (e: KeyboardEvent<HTMLInputElement>) => {
         if (e.key === 'Backspace') {
-            setNewSchool({
-                ...newSchool,
-                school_first_time_pass_rate: {
-                    ...newSchool.school_first_time_pass_rate,
-                    input: newSchool.school_first_time_pass_rate.input.replace('%', '')
-                }
-            })
+            if (firstSelected) {
+                setNewSchool({
+                    ...newSchool,
+                    school_first_time_pass_rate: {
+                        ...newSchool.school_first_time_pass_rate,
+                        input: newSchool.school_first_time_pass_rate.input.replace(firstSelected, '')
+                    }
+                })
+            } else {
+                setNewSchool({
+                    ...newSchool,
+                    school_first_time_pass_rate: {
+                        ...newSchool.school_first_time_pass_rate,
+                        input: newSchool.school_first_time_pass_rate.input.replace('%', '')
+                    }
+                })
+            }
+            setFirstSelected('')
         }
     }
 
     const keyDownFive = (e: KeyboardEvent<HTMLInputElement>) => {
         if (e.key === 'Backspace') {
-            setNewSchool({
-                ...newSchool,
-                school_average_five_year_first_time_pass_rate: {
-                    ...newSchool.school_average_five_year_first_time_pass_rate,
-                    input: newSchool.school_average_five_year_first_time_pass_rate.input.replace('%', '')
-                }
-            })
+            if (fiveSelected) {
+                setNewSchool({
+                    ...newSchool,
+                    school_average_five_year_first_time_pass_rate: {
+                        ...newSchool.school_average_five_year_first_time_pass_rate,
+                        input: newSchool.school_average_five_year_first_time_pass_rate.input.replace(fiveSelected, '')
+                    }
+                })
+            } else {
+                setNewSchool({
+                    ...newSchool,
+                    school_average_five_year_first_time_pass_rate: {
+                        ...newSchool.school_average_five_year_first_time_pass_rate,
+                        input: newSchool.school_average_five_year_first_time_pass_rate.input.replace('%', '')
+                    }
+                })
+            }
+            setFiveSelected('')
         }
     }
+
+    const mouseUp = () => {
+        const selection = window.getSelection();
+        if (selection) {
+            const selectedValue = selection.toString();
+            setFirstSelected(selectedValue)
+        }
+    }
+
+    const mouseUp2 = () => {
+        const selection = window.getSelection();
+        if (selection) {
+            const selectedValue = selection.toString();
+            setFiveSelected(selectedValue)
+        }
+    }
+
+    console.log(firstSelected, fiveSelected)
 
 
     // const addPercentage = (e:any) => {
@@ -145,11 +188,11 @@ export default function PANCEPassRate({newSchool, setNewSchool}: { newSchool: Sc
 
     return (
         <>
-        <div className={`mt-10 relative max-w-[900px] border-2 p-4 block rounded border-[#B4B4B4]`}>
+        <div className={`mt-10 relative max-w-[900px] border-2 p-4 block rounded border-[#B4B4B4]`} onMouseUp={mouseUp}>
                 <label className="absolute top-[-16px] text-xl bg-white">First Time Pass Rate</label>
                 <div className='flex justify-center items-center gap-3'>
                     <input className="grow focus:outline-none border border-[#B4B4B4] p-3 rounded" 
-                    value={newSchool.school_first_time_pass_rate.input} name='school_first_time_pass_rate' onChange={handleInput} onKeyDown={keyDownFirst}/>
+                    value={newSchool.school_first_time_pass_rate.input} name='school_first_time_pass_rate' onChange={handleInput} onKeyDown={keyDownFirst} />
                     <button onClick={(e:any) => {toggleNotePopup(e); setName('school_first_time_pass_rate')}} name='add' className="w-32 border text-[#F06A6A] border-[#F06A6A] rounded h-[50px] text-xl hover:text-white hover:bg-[#F06A6A]">
                         Add Note
                     </button>
@@ -179,11 +222,11 @@ export default function PANCEPassRate({newSchool, setNewSchool}: { newSchool: Sc
                 }
             </div>
 
-            <div className={`mt-12 relative max-w-[900px] border-2 p-4 block rounded border-[#B4B4B4]`}>
+            <div className={`mt-12 relative max-w-[900px] border-2 p-4 block rounded border-[#B4B4B4]`} onMouseUp={mouseUp2}>
                 <label className="absolute top-[-16px] text-xl bg-white">Five Year Average First-Time Pass Rate</label>
                 <div className='flex justify-center items-center gap-3'>
                     <input className="grow focus:outline-none border border-[#B4B4B4] p-3 rounded" 
-                    value={newSchool.school_average_five_year_first_time_pass_rate.input ? newSchool.school_average_five_year_first_time_pass_rate.input : ''} name='school_average_five_year_first_time_pass_rate' onChange={handleInput} onKeyDown={keyDownFive}/>
+                    value={newSchool.school_average_five_year_first_time_pass_rate.input ? newSchool.school_average_five_year_first_time_pass_rate.input : ''} name='school_average_five_year_first_time_pass_rate' onChange={handleInput} onKeyDown={keyDownFive} />
                     <button onClick={(e:any) => {toggleNotePopup(e); setName('school_average_five_year_first_time_pass_rate')}} name='add' className="w-32 border text-[#F06A6A] border-[#F06A6A] rounded h-[50px] text-xl hover:text-white hover:bg-[#F06A6A]">
                         Add Note
                     </button>
