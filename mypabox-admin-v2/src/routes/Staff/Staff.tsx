6e9bcf4cf89530e5ba64bf80bbe3bbec0851b0a3
Melanie Tojong <mtojong@Melanies-MBP.lan>
2023-12-06 20:22:58 -0500

    adding permissions ui

--- mypabox-admin-v2/src/routes/Staff/Staff.tsx
+++ mypabox-admin-v2/src/routes/Staff/Staff.tsx
@@ -46,28 +46,62 @@ export default function Staff() {
                             </div>
                             <button className='border-2 border-[#4573D2] text-[#4573D2] font-medium rounded px-3 py-1 hover:text-white hover:bg-[#4573D2]'>+ Add Task</button>
                         </div>
-                        <div className='w-full h-[45px] flex justify-between items-center border-b border-[#E5E5E5] relative' onClick={togglePermissions}>
-                            <div className='flex justify-start items-center absolute top-0 bottom-0 left-[12px]' onClick={togglePermissions}>
+                        <div className='w-full border-b border-[#E5E5E5] relative'>
+                            <div className='absolute top-0 bottom-0 right-0 left-0' onClick={togglePermissions}></div>
+                            <div className='flex justify-start items-center p-3'>
                                 {openPermissions ? <RxCaretDown className='mr-3 w-5 h-5 text-[#B4B4B4]' /> : <RxCaretRight className='mr-3 w-5 h-5 text-[#B4B4B4]'/>}
                                 <p className='text-sm'>{openPermissions ? 'Hide' : 'View'} Permissions</p>
                             </div>
+                            <div className='border border-[#A4A4A4] mx-3 mb-3 rounded'>
+                                <div className='p-3 border-b border-[#A4A4A4] flex justify-start items-center'>
+                                    <label className="relative inline-flex items-center cursor-pointer">
+                                        <input type="checkbox" className="sr-only peer" />
+                                        <div className="w-[36px] h-5 bg-gray-200 peer-focus:outline-none rounded-full shadow-inner peer dark:bg-gray-200 peer-checked:after:translate-x-[16px] after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-orange-600"></div>
+                                        <span className="ml-3 text-sm text-black">Edit input fields</span>
+                                    </label>
+                                </div>
+                                <div className='p-3 border-b border-[#A4A4A4] flex justify-start items-center'>
+                                    <label className="relative inline-flex items-center cursor-pointer">
+                                        <input type="checkbox" className="sr-only peer" />
+                                        <div className="w-[36px] h-5 bg-gray-200 peer-focus:outline-none rounded-full shadow-inner peer dark:bg-gray-200 peer-checked:after:translate-x-[16px] after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-orange-600"></div>
+                                        <span className="ml-3 text-sm text-black">Verify input fields</span>
+                                    </label>
+                                </div>
+                                <div className='p-3 border-b border-[#A4A4A4] flex justify-start items-center'>
+                                    <label className="relative inline-flex items-center cursor-pointer">
+                                        <input type="checkbox" className="sr-only peer" />
+                                        <div className="w-[36px] h-5 bg-gray-200 peer-focus:outline-none rounded-full shadow-inner peer dark:bg-gray-200 peer-checked:after:translate-x-[16px] after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-orange-600"></div>
+                                        <span className="ml-3 text-sm text-black">Make school data live</span>
+                                    </label>
+                                </div>
+                                <div className='p-3 flex justify-start items-center'>
+                                    <label className="relative inline-flex items-center cursor-pointer">
+                                        <input type="checkbox" className="sr-only peer" />
+                                        <div className="w-[36px] h-5 bg-gray-200 peer-focus:outline-none rounded-full shadow-inner peer dark:bg-gray-200 peer-checked:after:translate-x-[16px] after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-orange-600"></div>
+                                        <span className="ml-3 text-sm text-black">Add / delete school data</span>
+                                    </label>
+                                </div>
+                            </div>
                         </div>
-                        <div className='w-full h-[45px] flex justify-between items-center border-b border-[#E5E5E5] relative' onClick={toggleActive}>
-                            <div className='flex justify-start items-center absolute top-0 bottom-0 left-[12px]' onClick={toggleActive}>
+                        <div className='w-full border-b border-[#E5E5E5] relative'>
+                            <div className='absolute top-0 bottom-0 right-0 left-0' onClick={toggleActive}></div>
+                            <div className='flex justify-start items-center p-3'>
                                 {openActive ? <RxCaretDown className='mr-3 w-5 h-5 text-[#B4B4B4]' /> : <RxCaretRight className='mr-3 w-5 h-5 text-[#B4B4B4]'/>}
                                 <p className='text-sm mr-2'>{openActive ? 'Hide' : 'View'} Active Tasks</p>
                                 <p className='text-sm text-[#B4B4B4]'>(0)</p>
                             </div>
                         </div>
-                        <div className='w-full h-[45px] flex justify-between items-center border-b border-[#E5E5E5] relative' onClick={toggleCompleted}>
-                            <div className='flex justify-start items-center absolute top-0 bottom-0 left-[12px]' onClick={toggleCompleted}>
+                        <div className='w-full border-b border-[#E5E5E5] relative'>
+                            <div className='absolute top-0 bottom-0 right-0 left-0' onClick={toggleCompleted}></div>
+                            <div className='flex justify-start items-center p-3'>
                                 {openCompleted ? <RxCaretDown className='mr-3 w-5 h-5 text-[#B4B4B4]' /> : <RxCaretRight className='mr-3 w-5 h-5 text-[#B4B4B4]'/>}
                                 <p className='text-sm mr-2'>{openCompleted ? 'Hide' : 'View'} Completed Tasks</p>
                                 <p className='text-sm text-[#B4B4B4]'>(0)</p>
                             </div>
                         </div>
-                        <div className='w-full h-[45px] flex justify-between items-center border-b border-[#E5E5E5] relative' onClick={toggleArchived}>
-                            <div className='flex justify-start items-center absolute top-0 bottom-0 left-[12px]' onClick={toggleArchived}>
+                        <div className='w-full border-b border-[#E5E5E5] relative'>
+                            <div className='absolute top-0 bottom-0 right-0 left-0' onClick={toggleArchived}></div>
+                            <div className='flex justify-start items-center p-3'>
                                 {openArchived ? <RxCaretDown className='mr-3 w-5 h-5 text-[#B4B4B4]' /> : <RxCaretRight className='mr-3 w-5 h-5 text-[#B4B4B4]'/>}
                                 <p className='text-sm mr-2'>{openArchived ? 'Hide' : 'View'} Archived Tasks</p>
                                 <p className='text-sm text-[#B4B4B4]'>(0)</p>
